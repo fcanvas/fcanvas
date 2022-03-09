d70e83ef4543e86092c080c43b3a45a2ba7d61aa
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-09 21:50:53 +0700

    add shapes

---
+++ src/shapes/Circle.ts
@@ -0,0 +1,30 @@
+import { Shape, AttrsDefault } from "../Shape"
+import { TWO_PI } from "../constants/TWO_PI"
+import { pointInCircle } from "../helpers/pointInCircle"
+
+type Attrs = AttrsDefault & {
+	radius: number
+}
+
+export class Circle extends Shape<Attrs> {
+	readonly type = "Circle"
+	protected readonly attrsReactSize = ["radius"]
+	
+	protected _sceneFunc(context: CanvasRenderingContext2D) {
+		context.arc(this.attrs.radius / 2, this.attrs.radius / 2, this.attrs.radius, 0, TWO_PI)
+	}
+	constructor(attrs : Attrs) {
+		super(attrs)
+	}
+
+	public getInnerWidth() {
+		return this.attrs.radius * 2
+	}
+	public getInnerHeight() {
+		return this.attrs.radius * 2
+	}
+
+	protected isPressedPoint(x: number, y: number) {
+		return pointInCircle(x, y, this.attrs.x, this.attrs.y, this.attrs.radius)
+	}
+}
