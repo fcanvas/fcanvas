0d830533eb06e04dfc1f8bcadefd489d0c2d9e06
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-09 17:25:10 +0700

    update demo

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -8,10 +8,13 @@ const layer = new Layer()
 
 const circle = new Shape({
   sceneFunc(context) {
-    
+    console.log(context)
+    context.arc(50, 50, 50, 0, Math.PI * 2)
   },
   x:0,
-  y: 0
+  y: 0,
+  // fill: "red"
+  stroke: "black"
 })
 
 layer.add(circle)
