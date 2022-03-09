91484b2c8ec089a8aa69b581529937b86deb9f31
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-09 03:27:43 +0000

    first commit for from `fcanvas-core`

---
+++ src/helpers/pointInCircle.ts
@@ -0,0 +1,10 @@
+export function pointInCircle(
+  x0: number,
+  y0: number,
+  x: number,
+  y: number,
+  r: number
+): boolean {
+  const distancesquared = (x0 - x) ** 2 + (y0 - y) ** 2;
+  return distancesquared <= r ** 2;
+}
