d4c24e1d14f3b2d37bdc1546bcd3b7620488d28d
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-09 11:45:47 +0700

    add demo

---
+++ demo/src/main.ts
@@ -0,0 +1,20 @@
+import { Stage, Layer, Shape } from "../../src/index"
+
+const stage = new Stage({
+  container: "app"
+})
+
+const layer = new Layer()
+
+const circle = new Shape({
+  sceneFunc(context) {
+    
+  },
+  x:0,
+  y: 0
+})
+
+layer.add(circle)
+stage.add(layer)
+
+layer.draw()
