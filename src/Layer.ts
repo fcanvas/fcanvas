34544e3aea6b5321b508673a2199a093fe537c9a
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-10 09:49:22 +0000

    update api

--- src/Layer.ts
+++ src/Layer.ts
@@ -1,6 +1,9 @@
 import { Container } from "./Container";
 import { Shape } from "./Shape";
 
+type Attrs = {
+  clearBeforeDraw?: boolean
+}
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class Layer extends Container<Shape<any, any>> {
   readonly type = "Layer";
@@ -11,13 +14,27 @@ export class Layer extends Container<Shape<any, any>> {
     return this.#context.canvas;
   }
 
+  readonly #attrs: Attrs;
+  constructor(attrs: Attrs) {
+    super()
+
+    this.#attrs = attrs;
+  }
+
   public matches() {
     return false;
   }
 
-  draw() {
+  private _draw() {
+    if (this.#attrs.clearBeforeDraw ?? true) {
+      this.#context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
     this.children.forEach((node) => {
       node.draw(this.#context);
     });
+  }
+
+  draw() {
+    this._draw()
   }
 }
