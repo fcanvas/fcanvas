89780fb7f6d750d4ff8ef82d8a5e05e8d5d7f8d3
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-13 04:14:23 +0700

    update Layer

--- src/Layer.ts
+++ src/Layer.ts
@@ -1,10 +1,27 @@
 import { Container } from "./Container";
 import { EventsDefault, Shape } from "./Shape";
 import { realMousePosition } from "./helpers/realMousePosition";
+import { Offset} from "./types/Offset"
+import { OptionTransform, createTransform } from "./helpers/createTransform"
+import { OptionFilter, createFilter } from "./helpers/createFilter"
+import { createProxy } from "./helpers/createProxy";
 
-type Attrs = {
+type Attrs = Partial<Offset> & {
   // eslint-disable-next-line functional/prefer-readonly-type
   clearBeforeDraw?: boolean;
+  width?: number;
+  height?: number;
+  visible?: boolean
+  listening?: Map<string, Array<(event: any) => void>>
+  id?: string;
+  name?: string;
+  opacity?: number;
+  clip?: Offset & {
+    width: number
+    height: number
+  } | ((this: Layer, context: Path2D) => void);
+} & OptionTransform & {
+  filter?: OptionTransform
 };
 
 const EventsDefault = [
@@ -34,12 +51,78 @@ export class Layer extends Container<Shape<any, any>> {
   get canvas() {
     return this.#context.canvas;
   }
+  private currentNeedReload = true
 
-  readonly #attrs: Attrs;
+  readonly attrs: Attrs;
+  private reactOffset() : void {
+    this.canvas.style.left = (this.attrs.x ?? 0) + "px"
+    this.canvas.style.top = (this.attrs.y ?? 0) + "px"
+  }
+  private reactSize() : void {
+    this.canvas.style.width = this.attrs.width ? `${this.attrs.width}px` : "100%"
+    this.canvas.style.height = this.attrs.height ? `${this.attrs.height}px` : "100%"
+
+    ;[this.canvas.width, this.canvas.height] = [this.canvas.scrollWidth, this.canvas.scrollHeight]
+  }
+  private displayBp = ""
+  private reactVisible() : void {
+    this.displayBp = this.canvas.style.display
+    const display = getComputedStyle(this.canvas).getPropertyValue("display")
+
+    if (this.attrs.visible ?? true) {
+      if (display === "none") {
+        this.canvas.style.display = "block"
+      } else {
+        this.canvas.style.display = this.displayBp === "none" ? "" : this.displayBp
+      }
+
+      return
+    }
+
+    if (display === "none") {
+      return
+    }
+    this.canvas.style.display = "none"
+  }
+  private reactId() : void {
+    this.canvas.setAttribute("id", this.attrs.id ?? "")
+  }
+  private reactName() : void {
+    this.canvas.setAttribute("class", this.attrs.name ?? "")
+  }
+
   constructor(attrs: Attrs = {}) {
     super();
 
-    this.#attrs = attrs;
+    this.reactOffset()
+    this.reactSize()
+    this.reactVisible()
+    this.reactId()
+    this.reactName()
+    this.attrs = createProxy(attrs, prop => {
+      if (prop === "x" || prop === "y") {
+        this.reactOffset()
+        return
+      }
+      if (prop === "width" || prop === "height") {
+        this.reactSize()
+        return
+      }
+      if (prop === "visible") {
+        this.reactVisible()
+        return
+      }
+      if (prop === "id") {
+        this.reactId()
+        return
+      }
+      if (prop === "name") {
+        this.reactName()
+        return
+      }
+
+      this.currentNeedReload = true
+    });
     EventsDefault.forEach((type) => {
       this.canvas.addEventListener(
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -48,6 +131,7 @@ export class Layer extends Container<Shape<any, any>> {
       );
     });
   }
+
   private activatorEventChildren(
     event: EventsDefault[keyof EventsDefault]
   ): void {
@@ -79,12 +163,81 @@ export class Layer extends Container<Shape<any, any>> {
   }
 
   private _draw() {
-    if (this.#attrs.clearBeforeDraw ?? true) {
-      this.#context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    const needReload = this.currentNeedReload || Array.from(this.children).some(node => {
+      node.parentNeedReloading = false;
+      return node.parentNeedReloading
+    })
+
+    if (needReload === false) {
+      return
     }
+    
+    const context = this.#context
+
+    if (this.attrs.clearBeforeDraw ?? true) {
+      context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+    const needBackup = this.attrs.clip !== void 0
+
+    if (needBackup) {
+      context.save()
+
+      if (typeof this.attrs.clip === "function") {
+        const path = new Path2D()
+        this.attrs.clip.call(this, path)
+        context.clip(path)
+      } else {
+        context.rect(this.attrs.clip!.x, this.attrs.clip!.y, this.attrs.clip!.width, this.attrs.clip!.height)
+      }
+    }
+    const needUseTransform =
+      this.attrs.scale !== void 0 ||
+      this.attrs.rotation !== void 0 ||
+      this.attrs.offset !== void 0 ||
+      this.attrs.skewX !== void 0 ||
+      this.attrs.skewY !== void 0 ||
+      !context
+    const needSetAlpha = this.attrs.opacity !== void 0;
+    const useFilter = this.attrs.filter !== void 0
+    // eslint-disable-next-line functional/no-let
+    let backupTransform, backupAlpha: number, backupFilter: string;;
+
+    if (needSetAlpha) {
+      backupAlpha = context.globalAlpha;
+      // eslint-disable-next-line functional/immutable-data, @typescript-eslint/no-non-null-assertion
+      context.globalAlpha = this.attrs.opacity!;
+    }
+    if (needUseTransform) {
+      backupTransform = context.getTransform();
+
+      context.setTransform(createTransform(this.attrs, !context));
+    }
+    if (useFilter) {
+      backupFilter = context.filter
+
+      context.filter = createFilter(this.attrs.filter!) 
+    }
+
     this.children.forEach((node) => {
-      node.draw(this.#context);
+      node.draw(context);
     });
+
+    if (useFilter) {
+      // eslint-disable-next-line functional/immutable-data, @typescript-eslint/no-non-null-assertion
+      context.filter = backupFilter!
+    }
+    if (needUseTransform) {
+      context.setTransform(backupTransform);
+    }
+    if (needSetAlpha) {
+      // eslint-disable-next-line functional/immutable-data, @typescript-eslint/no-non-null-assertion
+      context.globalAlpha = backupAlpha!;
+    }
+    if (needBackup) {
+      context.restore()
+    }
+
+    this.currentNeedReload = false
   }
 
   draw() {
