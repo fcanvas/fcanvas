84cb2237d7353c5aa3c5f4169d88b311d515b9e9
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-13 07:08:33 +0000

    remove recordString

--- src/Stage.ts
+++ src/Stage.ts
@@ -9,13 +9,16 @@ type Attrs = {
   // eslint-disable-next-line functional/prefer-readonly-type
   container: string;
 };
-export class Stage extends Container<Layer> {
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Events = {};
+
+export class Stage extends Container<Attrs, Events, Layer> {
   readonly type = "Stage";
   readonly #attrs: Attrs;
   readonly container: HTMLElement;
 
   constructor(attrs: Attrs) {
-    super();
+    super(attrs);
     this.#attrs = attrs;
     const el = document.getElementById(attrs.container);
     if (!el) {
@@ -25,15 +28,22 @@ export class Stage extends Container<Layer> {
 
     this.container = el;
   }
-  add() {
-    // eslint-disable-next-line functional/functional-parameters, prefer-rest-params
-    super.add(...arguments);
+  // eslint-disable-next-line functional/functional-parameters, functional/prefer-readonly-type
+  add(...layers: Layer[]) {
+    super.add(...layers);
     this.children.forEach((layer) => {
       if (this.container.contains(layer.canvas) === false) {
         this.container.appendChild(layer.canvas);
       }
 
       layer.batchDraw();
+    });
+  }
+  // eslint-disable-next-line functional/functional-parameters, functional/prefer-readonly-type
+  delete(...layers: Layer[]) {
+    super.delete(...layers);
+    layers.forEach((layer) => {
+      layer.destroy();
     });
   }
 }
--- src/types/RecordString.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line functional/prefer-readonly-type
-export type RecordString<K extends string, V> = {
-  [prop in K]: V;
-};
