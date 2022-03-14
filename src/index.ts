22482087387ddbe8f7216db14e10c025b8cb0bbf
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-14 04:13:17 +0000

    update getTouchInfo

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,11 @@ export { Stage } from "./Stage";
 export { Layer } from "./Layer";
 export { Shape } from "./Shape";
 
+export { Arc    } from "./shapes/Arc"
 export { Circle } from "./shapes/Circle";
+export { Rect   } from "./shapes/Rect"
+
+
+export { degressToRadial } from "./utils/degressToRadial"
+export { radialToDegress } from "./utils/radialToDegress"
+
