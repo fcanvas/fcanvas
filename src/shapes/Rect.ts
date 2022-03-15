82043267fe558996d71e85387709191777738caa
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-15 11:31:57 +0000

    fix memory

--- src/shapes/Rect.ts
+++ src/shapes/Rect.ts
@@ -1,5 +1,4 @@
 import { Shape } from "../Shape";
-import { pointInBox } from "../helpers/pointInBox";
 
 type AttrsCustom = {
   // eslint-disable-next-line functional/prefer-readonly-type
@@ -17,16 +16,5 @@ export class Rect<EventsCustom extends Record<string, any> = {}> extends Shape<
 
   protected _sceneFunc(context: CanvasRenderingContext2D) {
     context.rect(0, 0, this.attrs.width, this.attrs.height);
-  }
-
-  public isPressedPoint(x: number, y: number) {
-    return pointInBox(
-      x,
-      y,
-      this.attrs.x,
-      this.attrs.y,
-      this.attrs.width,
-      this.attrs.height
-    );
   }
 }
