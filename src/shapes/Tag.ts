3136e144af08360a7dba499ff054f1d34dbd5019
45375496+tachibana-shin@users.noreply.github.com <45375496+tachibana-shin@users.noreply.github.com>
2022-03-17 14:23:13 +0700

    add Tag

---
+++ src/shapes/Tag.ts
@@ -0,0 +1,137 @@
+type AttrsCustom = {
+  pointerDirection?: "up" | "down" | "left" | "right" | "none";
+  pointerWidth?: number
+  pointerHeight?: number
+  cornerRadius?: number | number[];
+  width?: number;
+  height?: number;
+}
+
+export class Tag extends Shape {
+  protected _sceneFunc(context: CanvasRenderingContext2D) {
+    const width = this.attrs.width ?? 0,
+      height = this.attrs.height ?? 0,
+      pointerDirection = this.attrs.pointerDirection,
+      { pointerWidth ,
+      pointerHeight,
+      cornerRadius } = this.attrs;
+
+ 
+
+let topLeft = 0,
+        topRight = 0,
+        bottomRight = 0,
+        bottomLeft = 0;
+const ws2 = this.attrs.width / 2
+const hs2 = this.attrs.height / 2
+      if (typeof this.attrs.cornerRadius === "number") {
+        topLeft = Math.min(this.attrs.cornerRadius,ws2,hs2);
+        topRight = topLeft;
+        bottomRight = topLeft;
+        bottomLeft = topLeft;
+      } else if (this.attrs.cornerRadius.length === 2) {
+        topLeft = Math.min(this.attrs.cornerRadius[0],ws2,hs2)
+        bottomRight = topLeft;
+        topRight = Math. min(this.attrs.cornerRadius[1],ws2,hs2)
+        bottomLeft = topRight;
+      } else {
+        [topLeft, topRight, bottomRight, bottomLeft] = [Math.min(this.attrs.cornerRadius[0],ws2,hs2),Math.min(this.attrs.cornerRadius[1],ws2,hs2),Math.min(this.attrs.cornerRadius[2],ws2,hs2),Math.min(this.attrs.cornerRadius[3],ws2,hs2),];
+      }
+      
+
+  
+    context.moveTo(topLeft, 0);
+
+    if (pointerDirection === "up") {
+      context.lineTo((width - pointerWidth) / 2, 0);
+      context.lineTo(width / 2, -1 * pointerHeight);
+      context.lineTo((width + pointerWidth) / 2, 0);
+    }
+
+    context.lineTo(width - topRight, 0);
+    context.arc(
+      width - topRight,
+      topRight,
+      topRight,
+      (Math.PI * 3) / 2,
+      0,
+      false
+    );
+
+    if (pointerDirection === "right") {
+      context.lineTo(width, (height - pointerHeight) / 2);
+      context.lineTo(width + pointerWidth, height / 2);
+      context.lineTo(width, (height + pointerHeight) / 2);
+    }
+
+    context.lineTo(width, height - bottomRight);
+    context.arc(
+      width - bottomRight,
+      height - bottomRight,
+      bottomRight,
+      0,
+      Math.PI / 2,
+      false
+    );
+
+    if (pointerDirection === "down") {
+      context.lineTo((width + pointerWidth) / 2, height);
+      context.lineTo(width / 2, height + pointerHeight);
+      context.lineTo((width - pointerWidth) / 2, height);
+    }
+
+    context.lineTo(bottomLeft, height);
+    context.arc(
+      bottomLeft,
+      height - bottomLeft,
+      bottomLeft,
+      Math.PI / 2,
+      Math.PI,
+      false
+    );
+
+    if (pointerDirection === "left") {
+      context.lineTo(0, (height + pointerHeight) / 2);
+      context.lineTo(-1 * pointerWidth, height / 2);
+      context.lineTo(0, (height - pointerHeight) / 2);
+    }
+
+    context.lineTo(0, topLeft);
+    context.arc(topLeft, topLeft, topLeft, Math.PI, (Math.PI * 3) / 2, false);
+
+    this.fillStrokeScene(context);
+  }
+  public getSelfRect() {
+    const x = 0,
+      y = 0,
+      {pointerWidth,
+      pointerHeight,
+      direction} = this.attrs,
+      width = this.attrs.width ?? 0,
+      height = this.attrs.height ?? 0;
+
+    switch (direction ) {
+      case "up":
+      y -= pointerHeight;
+      height += pointerHeight;
+      break
+    case "down":
+      height += pointerHeight;
+      break
+    case "left":
+      // ARGH!!! I have no idea why should I used magic 1.5!!!!!!!!!
+      x -= pointerWidth * 1.5;
+      width += pointerWidth;
+      break
+    case "right":
+      width += pointerWidth * 1.5;
+      break
+    }
+    return {
+      x: x,
+      y: y,
+      width: width,
+      height: height,
+    };
+  }
+}
