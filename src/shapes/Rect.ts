8348c91b3cb5f3994b5e25f644fe84e3c7d5daa2
45375496+tachibana-shin@users.noreply.github.com <45375496+tachibana-shin@users.noreply.github.com>
2022-03-17 14:23:13 +0700

    add Tag

--- src/shapes/Rect.ts
+++ src/shapes/Rect.ts
@@ -23,21 +23,22 @@ export class Rect<EventsCustom extends Record<string, any> = {}> extends Shape<
         r2 = 0,
         r3 = 0,
         r4 = 0;
-
+const ws2 = this.attrs.width / 2
+const hs2 = this.attrs.height / 2
       if (typeof this.attrs.cornerRadius === "number") {
-        r1 = this.attrs.cornerRadius;
+        r1 = Math.min(this.attrs.cornerRadius,ws2,hs2);
         r2 = r1;
         r3 = r1;
         r4 = r1;
       } else if (this.attrs.cornerRadius.length === 2) {
-        r1 = this.attrs.cornerRadius[0];
-        r3 = this.attrs.cornerRadius[0];
-        r2 = this.attrs.cornerRadius[1];
-        r4 = this.attrs.cornerRadius[1];
+        r1 = Math.min(this.attrs.cornerRadius[0],ws2,hs2)
+        r3 = r1;
+        r2 = Math. min(this.attrs.cornerRadius[1],ws2,hs2)
+        r4 = r2;
       } else {
-        [r1, r2, r3, r4] = this.attrs.cornerRadius;
+        [r1, r2, r3, r4] = [Math.min(this.attrs.cornerRadius[0],ws2,hs2),Math.min(this.attrs.cornerRadius[1],ws2,hs2),Math.min(this.attrs.cornerRadius[2],ws2,hs2),Math.min(this.attrs.cornerRadius[3],ws2,hs2),];
       }
-
+      
       context.moveTo(0, 0);
       context.arcTo(
         this.attrs.width,
