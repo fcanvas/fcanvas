61e2b574e29a54342a372f58726341733a95dce7
45375496+tachibana-shin@users.noreply.github.com <45375496+tachibana-shin@users.noreply.github.com>
2022-03-17 12:03:48 +0700

    add Ring

--- src/shapes/Arc.ts
+++ src/shapes/Arc.ts
@@ -2,18 +2,11 @@ import { Shape } from "../Shape";
 import { convertToRadial } from "../helpers/convertToRadial";
 import { pointInCircle } from "../helpers/pointInCircle";
 
-type AttrsCustom = {
+export type AttrsCustom = {
   // eslint-disable-next-line functional/prefer-readonly-type
-  angle:
-    | number
-    | {
-        // eslint-disable-next-line functional/prefer-readonly-type
-        start: number;
-        // eslint-disable-next-line functional/prefer-readonly-type
-        end: number;
-      };
+  angle: number
   // eslint-disable-next-line functional/prefer-readonly-type
-  innerRadius?: number;
+  innerRadius: number;
   // eslint-disable-next-line functional/prefer-readonly-type
   outerRadius: number;
   // eslint-disable-next-line functional/prefer-readonly-type
@@ -22,9 +15,11 @@ type AttrsCustom = {
 
 const HALF_PI = Math.PI / 2;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-export class Arc<EventsCustom extends Record<string, any> = {}> extends Shape<
-  AttrsCustom,
-  EventsCustom
+export class Arc<EventsCustom extends Record<string, any> = {}, 
+  AttrsCustomMore extends Record<string, unknown> &
+    Omit<AttrsCustom, "angle"> = AttrsCustom> extends Shape<
+  AttrsCustomMore,
+  EventsCustom,
 > {
   static readonly type = "Arc";
   static readonly attrsReactSize = [
@@ -36,50 +31,11 @@ export class Arc<EventsCustom extends Record<string, any> = {}> extends Shape<
   public readonly _centroid = true;
 
   protected _sceneFunc(context: CanvasRenderingContext2D) {
-    const angleStart = convertToRadial(
-      typeof this.attrs.angle === "object" ? this.attrs.angle.start : 0
-    );
-    const angleEnd = convertToRadial(
-      typeof this.attrs.angle === "object"
-        ? this.attrs.angle.end
-        : this.attrs.angle
-    );
-
-    const sinStart = Math.sin(angleStart + HALF_PI);
-    const cosStart = Math.cos(angleStart + HALF_PI);
-
-    const sinEnd = Math.sin(angleEnd + HALF_PI);
-    const cosEnd = Math.cos(angleEnd + HALF_PI);
-
-    context.moveTo(
-      sinStart * (this.attrs.innerRadius ?? 0),
-      cosStart * (this.attrs.innerRadius ?? 0)
-    );
-    context.lineTo(
-      sinStart * this.attrs.outerRadius,
-      cosStart * this.attrs.outerRadius
-    );
-
-    if (this.attrs.innerRadius !== void 0) {
-      context.arc(
-        0,
-        0,
-        this.attrs.innerRadius,
-        angleStart,
-        angleEnd,
-        this.attrs.clockwise
-      );
-    }
-
-    context.moveTo(
-      sinEnd * (this.attrs.innerRadius ?? 0),
-      -cosEnd * this.attrs.outerRadius
-    );
-    context.lineTo(
-      +sinEnd * (this.attrs.innerRadius ?? 0),
-      -cosEnd * this.attrs.outerRadius
-    );
-
+    const angle = convertToRadial(this.attrs.angle)
+  
+    context.arc(0, 0, this.attrs.outerRadius, 0, angle, this.attrs.clockwise);
+    context.arc(0, 0, this.attrs.innerRadius, angle, 0, !this.attrs.clockwise);
+    
     this.fillStrokeScene(context);
   }
 
@@ -91,48 +47,6 @@ export class Arc<EventsCustom extends Record<string, any> = {}> extends Shape<
   }
 
   public isPressedPoint(x: number, y: number) {
-    const angle = Math.atan2(x - this.attrs.x, y - this.attrs.y);
-    const inAngle =
-      HALF_PI +
-        convertToRadial(
-          typeof this.attrs.angle === "object" ? this.attrs.angle.start : 0
-        ) <=
-        angle &&
-      HALF_PI +
-        convertToRadial(
-          typeof this.attrs.angle === "object"
-            ? this.attrs.angle.end
-            : this.attrs.angle
-        ) >=
-        angle;
-
-    if (!inAngle) return false;
-
-    if (this.attrs.outerRadius !== void 0) {
-      const inInnerBox = pointInCircle(
-        x,
-        y,
-        this.attrs.x,
-        this.attrs.y,
-        this.attrs.outerRadius
-      );
-      const inOuterBox = pointInCircle(
-        x,
-        y,
-        this.attrs.x,
-        this.attrs.y,
-        this.attrs.innerRadius ?? 0
-      );
-
-      return inInnerBox === false && inOuterBox;
-    }
-
-    return pointInCircle(
-      x,
-      y,
-      this.attrs.x,
-      this.attrs.y,
-      this.attrs.innerRadius ?? 0
-    );
+    return !pointInCircle(x, y, this.attrs.x, this.attrs.y, this.attrs.innerRadius) && pointInCircle(x, y, this.attrs.x, this.attrs.y, this.attrs.outerRadius)
   }
 }
