287cfbd06fa931ecd43595147957d3b906b1d4f2
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-03-24 19:40:24 +0700

    add utils function

---
+++ src/methods/haveIntersection.ts
@@ -0,0 +1,14 @@
+import { Offset } from "../types/Offset";
+import { Size } from "../types/Size";
+
+export function haveIntersection(
+  r1: Offset & Size,
+  r2: Offset & Size
+): boolean {
+  return !(
+    r2.x > r1.x + r1.width ||
+    r2.x + r2.width < r1.x ||
+    r2.y > r1.y + r1.height ||
+    r2.y + r2.height < r1.y
+  );
+}
---
+++ src/methods/randomColor.ts
@@ -0,0 +1,9 @@
+export function randomColor(): string {
+  // eslint-disable-next-line functional/no-let
+  let randColor = ((Math.random() * 0xffffff) << 0).toString(16);
+  // eslint-disable-next-line functional/no-loop-statement
+  while (randColor.length < 6) {
+    randColor = "0" + randColor;
+  }
+  return `#${randColor}`;
+}
