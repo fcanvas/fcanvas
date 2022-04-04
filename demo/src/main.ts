b2a74a6b007eebd66b3c0c79de6e0bac8a9465dd
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-04-04 05:38:43 +0000

    fix offset leak

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -2,16 +2,16 @@ import { Stage, Layer, Circle } from "../../src/index";
 
 const stage = new Stage({
   container: "app",
-  width: 400,
-  height: 400,
+  width: 310,
+  height: 310,
 });
 
 const layer = new Layer();
 stage.add(layer)
 
 layer.add(new Circle({
-    x: 160,
-    y: 160,
+    x: 151,
+    y: 151,
     radius: 150,
     stroke: "black"
 }))
