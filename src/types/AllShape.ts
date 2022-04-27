0d516517915815c70fb495fba71444884c4062f8
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-04-27 15:55:32 +0700

    lint

--- src/types/AllShape.ts
+++ src/types/AllShape.ts
@@ -1,6 +1,6 @@
-import { Shape } from "../Shape"
+import { Shape } from "../Shape";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type AllShape = Shape<any, any, any, any, any>
+type AllShape = Shape<any, any, any, any, any>;
 
-export default AllShape
+export default AllShape;
