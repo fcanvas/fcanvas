46987ae557d45619d491e1662d347b5a48269ad0
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-08 10:39:50 +0000

    update `onCollide`

--- packages/rcanvas/src/on/onCollide.ts
+++ packages/rcanvas/src/on/onCollide.ts
@@ -1,22 +1,50 @@
 import { watchPostEffect } from "vue"
 
 import type { Shape } from "../Shape"
+import { getCurrentShape } from "../currentShape"
 import { haveIntersection } from "../methods/haveIntersection"
 import type { BOUNCE_CLIENT_RECT } from "../symbols"
 
-export function onCollide<T extends Pick<Shape, typeof BOUNCE_CLIENT_RECT>>(
+interface Checks<T> {
+  forEach: (cb: (el: T, index: number) => void) => void
+}
+interface Cb<T> {
+  (el: T, index: number): void
+}
+
+function onCollide<T extends Pick<Shape, typeof BOUNCE_CLIENT_RECT>>(
+  checks: Checks<T>,
+  cb: Cb<T>
+): void
+// eslint-disable-next-line no-redeclare
+function onCollide<T extends Pick<Shape, typeof BOUNCE_CLIENT_RECT>>(
   target: Shape,
-  checks: {
-    forEach: (cb: (el: T, index: number) => void) => void
-  },
-  cb: (el: T, index: number) => void
+  checks: Checks<T>,
+  cb: Cb<T>
+): void
+
+// eslint-disable-next-line no-redeclare
+function onCollide<T extends Pick<Shape, typeof BOUNCE_CLIENT_RECT>>(
+  target: Shape | Checks<T>,
+  checks: Checks<T> | Cb<T>,
+  cb?: Cb<T>
 ) {
+  if (cb === undefined) {
+    ;[target, checks, cb] = [
+      getCurrentShape(),
+      target as Checks<T>,
+      checks as Cb<T>
+    ]
+  }
+
   return watchPostEffect(() => {
-    checks.forEach((el, index) => {
-      if (haveIntersection(target, el)) {
+    ;(checks as Checks<T>).forEach((el, index) => {
+      if (haveIntersection(target as Shape, el)) {
         // ok
-        cb(el, index)
+        ;(cb as Cb<T>)(el, index)
       }
     })
   })
 }
+
+export { onCollide }
