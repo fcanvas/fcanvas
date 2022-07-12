88e4adc3c623529f88be5ebaaaeda845d1ce9c54
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-12 13:25:10 +0000

    add build `browser`

--- packages/fcanvas/tsup.config.ts
+++ packages/fcanvas/tsup.config.ts
@@ -1,10 +1,29 @@
-import { defineConfig } from "tsup"
+import { defineConfig, Options } from "tsup"
 
-export default defineConfig({
+const configNormal: Options = {
   entry: ["src/index.ts"],
   clean: true,
   splitting: true,
   dts: true,
   format: ["cjs", "esm", "iife"],
   target: "es2015"
-})
+}
+const configBrowser: Options = {
+  entry: {
+    "index.browser": "src/index.ts"
+  },
+  dts: false,
+  splitting: true,
+  format: ["esm"],
+  target: "es2015",
+  noExternal: ["@vue/reactivity", "@vue/shared", "@vue-reactivity/watch", "gsap", "path-normalize"]
+}
+const configBrowserMinify: Options = {
+  ...configBrowser,
+  entry: {
+    "index.browser.min": "src/index.ts"
+  },
+  minify: true,
+}
+
+export default defineConfig([configNormal, configBrowser, configBrowserMinify])
