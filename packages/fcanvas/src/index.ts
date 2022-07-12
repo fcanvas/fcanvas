6b255aa676538c34834b0c212127966bb0c50271
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-12 14:43:35 +0000

    auto export

--- packages/fcanvas/src/index.ts
+++ packages/fcanvas/src/index.ts
@@ -7,41 +7,6 @@ import { Vector } from "./Vector"
 import { getCurrentShape } from "./currentShape"
 import { globalConfigs } from "./globalConfigs"
 import { hookEvent } from "./hookEvent"
-import { clamp } from "./methods/clamp"
-import { constrain } from "./methods/constrain"
-import { cropImage } from "./methods/cropImage"
-import { getMousePos } from "./methods/getMousePos"
-import { haveIntersection } from "./methods/haveIntersection"
-import { inRange } from "./methods/inRange"
-import { lerp } from "./methods/lerp"
-import { loadFont } from "./methods/loadFont"
-import { loadImage } from "./methods/loadImage"
-import { map } from "./methods/map"
-import { normalize } from "./methods/normalize"
-import { random } from "./methods/random"
-import { randomColor } from "./methods/randomColor"
-import { range } from "./methods/range"
-import { Arc } from "./shapes/Arc"
-import { Arrow } from "./shapes/Arrow"
-import { Circle } from "./shapes/Circle"
-import { Ellipse } from "./shapes/Ellipse"
-import { Image } from "./shapes/Image"
-import { Label } from "./shapes/Label"
-import { Line } from "./shapes/Line"
-import { Path } from "./shapes/Path"
-import { Rect } from "./shapes/Rect"
-import { RegularPolygon } from "./shapes/RegularPolygon"
-import { Ring } from "./shapes/Ring"
-import { Sprite } from "./shapes/Sprite"
-import { Star } from "./shapes/Star"
-import { Tag } from "./shapes/Tag"
-import { Text } from "./shapes/Text"
-import { TextPath } from "./shapes/TextPath"
-import { Wedge } from "./shapes/Wedge"
-import { useClientActivated } from "./useApi/useClientActivated"
-import { useEvent } from "./useApi/useEvent"
-import { useMouseIsPressed } from "./useApi/useMouseIsPressed"
-import { useMousePos } from "./useApi/useMousePos"
 
 export * from "@vue/reactivity"
 export * from "@vue-reactivity/watch"
@@ -61,50 +26,5 @@ export {
   Vector
 }
 
-// shapes
-export {
-  Arc,
-  Arrow,
-  Circle,
-  Ellipse,
-  Image,
-  Label,
-  Line,
-  Path,
-  Rect,
-  RegularPolygon,
-  Ring,
-  Sprite,
-  Star,
-  Tag,
-  Text,
-  TextPath,
-  Wedge
-}
-
-// methods
-export {
-  clamp,
-  constrain,
-  cropImage,
-  getMousePos,
-  haveIntersection,
-  inRange,
-  lerp,
-  loadFont,
-  loadImage,
-  map,
-  normalize,
-  random,
-  randomColor,
-  range
-}
-
 export * from "./symbols"
-// use apis
-export { useClientActivated, useEvent, useMouseIsPressed, useMousePos }
-
-export { onCollide } from "./on/onCollide"
-export { onMouseDown } from "./on/onMouseDown"
-export { onMouseMove } from "./on/onMouseMove"
-export { onMouseUp } from "./on/onMouseUp"
+export * from "./auto-export"
