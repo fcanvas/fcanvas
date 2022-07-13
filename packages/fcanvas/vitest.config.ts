62d3c3ac7cd0613c352d7819f348ba494b9465a9
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-13 17:08:51 +0700

    Update vitest.config.ts

--- packages/fcanvas/vitest.config.ts
+++ packages/fcanvas/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
-    environment: "jsdom" // or 'jsdom', 'node'
+    // environment: "jsdom" // or 'jsdom', 'node'
   }
 })
