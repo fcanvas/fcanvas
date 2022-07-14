7be8b904ecb99f351dfa3759e94354820f4aa29b
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-14 11:56:17 +0000

    add `Arc.spec.ts`

---
+++ packages/fcanvas/src/shapes/Arc.spec.ts
@@ -0,0 +1,88 @@
+// @vitest-environment jsdom
+
+import "./test/setup-environment"
+
+import { describe, expect, test } from "vitest"
+
+import { Layer } from "../Layer"
+import { Stage } from "../Stage"
+
+import { Arc } from "./Arc"
+import { compareLayerWithImage } from "./test/helpers/compareLayerWithImage"
+
+describe("Arc", () => {
+  test("add arc", async () => {
+    const stage = new Stage({ autoDraw: false })
+    const layer = new Layer()
+    const arc = new Arc({
+      x: 100,
+      y: 100,
+      innerRadius: 50,
+      outerRadius: 80,
+      angle: 90,
+      fill: "green",
+      stroke: "black",
+      strokeWidth: 4
+    })
+
+    layer.add(arc)
+    stage.add(layer)
+
+    layer.draw()
+
+    expect(
+      await compareLayerWithImage(layer, "arc_50-80_fill:green.png")
+    ).toEqual(true)
+  })
+
+  test("getSelfRect", () => {
+    const stage = new Stage({ autoDraw: false })
+    const layer = new Layer()
+    const arc = new Arc({
+      x: 100,
+      y: 100,
+      innerRadius: 50,
+      outerRadius: 80,
+      angle: 90,
+      fill: "green",
+      stroke: "black",
+      strokeWidth: 4
+    })
+
+    layer.add(arc)
+    stage.add(layer)
+
+    expect(arc.getBoundingClientRect()).toEqual({
+      width: 164,
+      height: 164,
+      x: -82,
+      y: -82
+    })
+  })
+
+  test("getSelfRect on clockwise", () => {
+    const stage = new Stage({ autoDraw: false })
+    const layer = new Layer()
+    const arc = new Arc({
+      x: 100,
+      y: 100,
+      innerRadius: 50,
+      outerRadius: 80,
+      angle: 90,
+      fill: "green",
+      stroke: "black",
+      strokeWidth: 4,
+      clockwise: true
+    })
+
+    layer.add(arc)
+    stage.add(layer)
+
+    expect(arc.getBoundingClientRect()).toEqual({
+      width: 164,
+      height: 164,
+      x: -82,
+      y: -82
+    })
+  })
+})
---
+++ packages/fcanvas/src/shapes/test/helpers/saveLayer.ts
@@ -0,0 +1,27 @@
+import { writeFileSync } from "fs"
+import { join } from "path"
+
+function dataURItoBlob(dataURI: string) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  const byteString = atob(dataURI.split(",")[1])
+
+  // write the bytes of the string to an ArrayBuffer
+  const ab = new ArrayBuffer(byteString.length)
+
+  // create a view into the buffer
+  const ia = new Uint8Array(ab)
+
+  // set the bytes of the buffer to the correct values
+  // eslint-disable-next-line functional/no-let, functional/immutable-data
+  for (let i = 0; i < byteString.length; i++) ia[i] = byteString.charCodeAt(i)
+
+  return ab
+}
+
+export function saveLayer(data: string, filename: string) {
+  writeFileSync(
+    join(__dirname, "..", "assets", filename),
+    Buffer.from(dataURItoBlob(data))
+  )
+}
