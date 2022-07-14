e5fd5d73b717b27defcf244bc2a45f9bb82c0e7d
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-14 07:07:22 +0000

    add `withinBounds` fn `map`

--- packages/fcanvas/src/methods/map.ts
+++ packages/fcanvas/src/methods/map.ts
@@ -1,9 +1,18 @@
+import { clamp } from "./clamp"
+
 export function map(
   value: number,
   start: number,
   stop: number,
   min: number,
-  max: number
+  max: number,
+  withinBounds?: boolean
 ): number {
-  return ((value - start) * (max - min)) / (stop - start) + min
+  const newVal = ((value - start) * (max - min)) / (stop - start) + min
+
+  if (!withinBounds) return newVal
+
+  if (min < max) return clamp(newVal, min, max)
+
+  return clamp(newVal, max, min)
 }
