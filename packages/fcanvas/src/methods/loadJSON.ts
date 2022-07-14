8bfbbea48e0ee5ac23ef274eacd80b2189c504ae
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-14 04:38:57 +0000

    add `loadJSON`

---
+++ packages/fcanvas/src/methods/loadJSON.ts
@@ -0,0 +1,33 @@
+import normalize from "path-normalize"
+
+// cache
+const fetchJSONMap = new Map<string, string>()
+function loadJSON(url: string, get?: true): Promise<string>
+function loadJSON(url: string, get: false): string
+
+function loadJSON(url: string, get = false): Promise<string> | string {
+  if (get) return getJSON(url)
+
+  const key = normalize(url)
+  const inCache = fetchJSONMap.get(key)
+
+  if (inCache) return Promise.resolve(inCache)
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((text) => {
+      fetchJSONMap.set(key, text)
+
+      return text
+    })
+}
+
+export { loadJSON }
+export function getJSON(url: string): string {
+  if (!fetchJSONMap.has(normalize(url)))
+    // eslint-disable-next-line functional/no-throw-statement
+    throw new Error(`Cannot find image ${url}. First run await loadJSON().`)
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return fetchJSONMap.get(normalize(url))!
+}
