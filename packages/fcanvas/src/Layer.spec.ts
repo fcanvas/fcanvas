f5cdbdb4a666922a92bc8fc17b2d3e59ac3a4d21
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-08-06 02:17:49 +0000

    lint

--- packages/fcanvas/src/Layer.spec.ts
+++ packages/fcanvas/src/Layer.spec.ts
@@ -1,4 +1,5 @@
-import { describe, test, expect } from "vitest"
+import { describe, expect, test } from "vitest"
+
 import { Layer } from "./Layer"
 import { Stage } from "./Stage"
 import { CANVAS_ELEMENT } from "./symbols"
