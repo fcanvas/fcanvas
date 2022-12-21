157a30f28da44244153f98895a6f42239baa1b03
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-12-21 12:17:49 +0000

    typo

--- packages/fcanvas/src/env.ts
+++ packages/fcanvas/src/env.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const isDev = (import.meta as unknown as any)?.env.IS_BROWSER
+// eslint-disable-next-line no-undef
+export const isDev = false
