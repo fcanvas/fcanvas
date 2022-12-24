448bb885946cc7698cf2ccb35c6ca54245fc84ac
Tachibana Shin <tachibshin@duck.com>
2022-12-24 13:02:38 +0000

    add `ImageRepeat`

--- packages/fcanvas/src/shapes/ImageRepeat.ts
+++ packages/fcanvas/src/shapes/ImageRepeat.ts
@@ -1,73 +1,74 @@
-import type { ComputedRef, reactive } from "@vue/reactivity";
-import { computed } from "@vue/reactivity";
 import { watch } from "@vue-reactivity/watch"
+import type { ComputedRef, reactive } from "@vue/reactivity"
+import { computed } from "@vue/reactivity"
 
-import { Shape } from "../Shape";
-import { getImage, loadImage } from "../methods/loadImage";
-import { SCOPE } from "../symbols";
-import type { CommonShapeAttrs } from "../type/CommonShapeAttrs";
-import type { Rect } from "../type/Rect";
-import type { ReactiveType } from "../type/fn/ReactiveType";
+import { Shape } from "../Shape"
+import { isDev } from "../env"
+import { getImage, loadImage } from "../methods/loadImage"
+import { SCOPE } from "../symbols"
+import type { CommonShapeAttrs } from "../type/CommonShapeAttrs"
+import type { Rect } from "../type/Rect"
+import type { ReactiveType } from "../type/fn/ReactiveType"
+
+import { getValFromSource } from "./Image"
 
 type PersonalAttrs = {
-  // eslint-disable-next-line no-undef
-  image: CanvasImageSource | string;
-  crop?: Rect;
+  image: CanvasImageSource | string
+  crop?: Rect
 } & Partial<{
-  width: number;
-  height: number;
+  width: number
+  height: number
 
-  scrollWidth: number;
-  scrollHeight: number;
-  scrollTop: number;
-  scrollLeft: number;
+  scrollWidth: number
+  scrollHeight: number
+  scrollTop: number
+  scrollLeft: number
 
-  whileDraw: boolean;
-}>;
-
-function getValFromSource(val: SVGAnimatedLength | number) {
-  if (typeof val === "number") return val;
-
-  return val.baseVal.value;
-}
+  whileDraw: boolean
+}>
 
 export class ImageRepeat extends Shape<PersonalAttrs> {
-  static readonly type = "Image";
-  static readonly fromURL = loadImage;
+  static readonly type = "ImageRepeat"
+  static readonly fromURL = loadImage
 
-  // eslint-disable-next-line no-undef
-  private readonly _image: ComputedRef<CanvasImageSource>;
+  private readonly _image: ComputedRef<CanvasImageSource>
   private readonly _cacheImageRepeat: Map<
     string,
     {
-      r: HTMLCanvasElement;
-      t: number;
-      l: number;
+      r: HTMLCanvasElement
+      t: number
+      l: number
     }
-  > = new Map();
+  > = new Map()
 
   private createImageRepeat(
-    image: HTMLCanvasElement | HTMLImageElement,
+    image: CanvasImageSource,
+    imageWidth: number,
+    imageHeight: number,
     scrollTop?: number,
     scrollLeft?: number,
     scrollWidth?: number,
     scrollHeight?: number,
     cache?: false
-  ): HTMLCanvasElement;
+  ): HTMLCanvasElement
   private createImageRepeat(
-    image: HTMLCanvasElement | HTMLImageElement,
+    image: CanvasImageSource,
+    imageWidth: number,
+    imageHeight: number,
     scrollTop?: number,
     scrollLeft?: number,
     scrollWidth?: number,
     scrollHeight?: number,
-    cache: true
+    cache?: true
   ): {
-    r: HTMLCanvasElement;
-    t: number;
-    l: number;
-  };
+    r: HTMLCanvasElement
+    t: number
+    l: number
+  }
   private createImageRepeat(
-    image: HTMLCanvasElement | HTMLImageElement,
+    image: CanvasImageSource,
+    imageWidth: number,
+    imageHeight: number,
     scrollTop?: number,
     scrollLeft?: number,
     scrollWidth?: number,
@@ -76,181 +77,170 @@ export class ImageRepeat extends Shape<PersonalAttrs> {
   ):
     | HTMLCanvasElement
     | {
-        r: HTMLCanvasElement;
-        t: number;
-        l: number;
+        r: HTMLCanvasElement
+        t: number
+        l: number
       } {
-    const { width: imageWidth, height: imageHeight } = image;
-
-    let id: string;
+    // eslint-disable-next-line functional/no-let
+    let id: string
     if (cache) {
-      id = `${scrollWidth}x${scrollHeight}`;
-      const inCache = this._cacheImageRepeat.get(id);
+      id = `${scrollWidth}x${scrollHeight}`
+      const inCache = this._cacheImageRepeat.get(id)
 
       if (inCache) {
-        console.log("[cache]: cache by createImageRepeat used!");
+        if (isDev) console.log("[cache]: cache by createImageRepeat used!")
         return {
           r: inCache.r,
-          t: scrollTop ? scrollTop - inCache.t : 0,
-          l: scrollLeft ? scrollLeft - inCache.l : 0,
-        };
+          t: scrollTop ? -scrollTop + inCache.t : 0,
+          l: scrollLeft ? -scrollLeft + inCache.l : 0
+        }
       }
     }
 
-    const ctx = document.createElement("canvas").getContext("2d");
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const ctx = document.createElement("canvas").getContext("2d")!
 
     if (cache) {
-      if (scrollWidth) scrollWidth += imageWidth * 2;
-      if (scrollHeight) scrollHeight += imageHeight * 2;
+      if (scrollWidth) scrollWidth += imageWidth * 2
+      if (scrollHeight) scrollHeight += imageHeight * 2
     }
 
-    ctx.canvas.width = scrollWidth ?? imageWidth;
-    ctx.canvas.height = scrollHeight ?? imageHeight;
+    ctx.canvas.width = scrollWidth ?? imageWidth
+    ctx.canvas.height = scrollHeight ?? imageHeight
 
-    let canvasRepeatX: CanvasRenderingContext2D | null = null;
+    // eslint-disable-next-line functional/no-let
+    let canvasRepeatX: CanvasRenderingContext2D | null = null
     if (scrollWidth) {
-      scrollLeft ??= 0;
-      const axisY = scrollHeight ? 0 : scrollTop ?? 0;
+      scrollLeft ??= 0
+      const axisY = scrollHeight ? 0 : -(scrollTop ?? 0)
 
-      let ctxScoop;
+      // eslint-disable-next-line functional/no-let
+      let ctxScoop: CanvasRenderingContext2D
       if (scrollHeight) {
-        canvasRepeatX = document.createElement("canvas").getContext("2d");
-        canvasRepeatX.canvas.width = scrollWidth;
-        canvasRepeatX.canvas.height = imageHeight;
-        ctxScoop = canvasRepeatX;
+        canvasRepeatX = document.createElement("canvas").getContext("2d")
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        canvasRepeatX!.canvas.width = scrollWidth
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        canvasRepeatX!.canvas.height = imageHeight
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        ctxScoop = canvasRepeatX!
       } else {
-        ctxScoop = ctx;
+        ctxScoop = ctx
       }
 
       // const offsetTop = imageWidth %
-      const offsetLeft = -(scrollLeft % imageWidth);
-      let currentX = offsetLeft;
-      const maxX = scrollWidth;
+      const offsetLeft = scrollLeft % imageWidth
+      // eslint-disable-next-line functional/no-let
+      let currentX = offsetLeft
+      const maxX = scrollWidth
 
       while (true) {
-        if (currentX > maxX) break;
+        if (currentX > maxX) break
 
-        ctxScoop.drawImage(image, currentX, axisY);
-        currentX += imageWidth;
+        ctxScoop.drawImage(image, currentX, axisY)
+        currentX += imageWidth
       }
     }
     if (scrollHeight) {
-      scrollTop ??= 0;
-      const axisX = scrollWidth ? 0 : scrollLeft ?? 0;
+      scrollTop ??= 0
+      const axisX = scrollWidth ? 0 : -(scrollLeft ?? 0)
 
-      const imageScoop = canvasRepeatX?.canvas ?? image;
+      const imageScoop = canvasRepeatX?.canvas ?? image
 
-      const offsetTop = -(scrollTop % imageHeight);
-      let currentY = offsetTop;
-      const maxY = scrollHeight;
+      const offsetTop = scrollTop % imageHeight
+      // eslint-disable-next-line functional/no-let
+      let currentY = offsetTop
+      const maxY = scrollHeight
 
       while (true) {
-        if (currentY > maxY) break;
+        if (currentY > maxY) break
 
-        ctx.drawImage(imageScoop, axisX, currentY);
-        currentY += imageHeight;
+        ctx.drawImage(imageScoop, axisX, currentY)
+        currentY += imageHeight
       }
     }
 
     if (cache) {
+      console.warn("sscae")
       const inCache = {
         r: ctx.canvas,
-        t: 0,
-        l: 0,
-      };
-      this._cacheImageRepeat.clear();
-      this._cacheImageRepeat.set(id, inCache);
+        t: scrollTop ?? 0,
+        l: scrollLeft ?? 0
+      }
+      this._cacheImageRepeat.clear()
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      this._cacheImageRepeat.set(id, inCache)
 
-      return inCache;
+      return inCache
     }
 
-    return ctx.canvas;
+    return ctx.canvas
   }
 
   protected _sceneFunc(context: CanvasRenderingContext2D) {
-    let image: HTMLCanvasElement | HTMLImageElement;
-    if (this.$.crop) {
-      image = document.createElement("canvas");
-      image.width = this.$.width ?? this.$.crop.width;
-      image.height = this.$.height ?? this.$.crop.height;
-      image
-        .getContext("2d")!
-        .drawImage(
-          this._image.value,
-          this.$.crop.x,
-          this.$.crop.y,
-          this.$.crop.width,
-          this.$.crop.height,
-          0,
-          0,
-          this.$.width ?? this.$.crop.width,
-          this.$.height ?? this.$.crop.height
-        );
-    } else if (this.$.width !== undefined && this.$.height !== undefined) {
-      image = document.createElement("canvas");
-      [image.width, image.height] = [this.$.width, this.$.height];
-      image
-        .getContext("2d")!
-        .drawImage(this._image.value, 0, 0, this.$.width, this.$.height);
-    } else {
-      image = this._image.value;
-      // image.drawImage(this._image.value, 0, 0);
-    }
+    const image = this._image.value
 
     // done. image (croped) ready!
-    this.fillStrokeScene(context);
+    this.fillStrokeScene(context)
 
-    const { width: imageWidth, height: imageHeight } = image;
+    const imageWidth = getValFromSource(image.width)
+    const imageHeight = getValFromSource(image.height)
 
     // image = 7 x 5
     // const scrollTop = 0;
     // const scrollLeft = 50;
     // const scrollWidth = imageWidth * 2;
     // const scrollHeight = imageHeight * 2;
-    const { scrollTop, scrollLeft, scrollWidth, scrollHeight } = this.$;
+    const {
+      scrollTop,
+      scrollLeft,
+      scrollWidth = imageWidth,
+      scrollHeight = imageHeight
+    } = this.$
 
     if (this.$.whileDraw !== true) {
+      // eslint-disable-next-line functional/no-let
       let {
         r,
         t: y,
-        l: x,
+        l: x
       } = this.createImageRepeat(
         image,
+        imageWidth,
+        imageHeight,
         scrollTop,
         scrollLeft,
         scrollWidth,
         scrollHeight,
         true
-      );
+      )
 
       if (x) {
-        if (x > 0) {
-          x = -r.width;
-        } else if (-x > scrollWidth) {
-          x = -imageWidth;
-        }
+        // x = 50
+        x %= imageWidth
+        x -= imageWidth
       }
       if (y) {
-        if (y > 0) {
-          y = -r.height;
-        } else if (-y > scrollHeight) {
-          y = -imageHeight;
-        }
+        y %= imageHeight
+        y -= imageHeight
       }
 
-      context.drawImage(r, x, y);
+      context.drawImage(r, x, y)
     } else {
       /// kfoprekgo
       const r = this.createImageRepeat(
         image,
+        imageWidth,
+        imageHeight,
         scrollTop,
         scrollLeft,
         scrollWidth,
         scrollHeight,
         false
-      );
+      )
 
-      context.drawImage(r, 0, 0);
+      context.drawImage(r, 0, 0)
     }
   }
 
@@ -259,25 +249,59 @@ export class ImageRepeat extends Shape<PersonalAttrs> {
       CommonShapeAttrs<PersonalAttrs> & {
         setup?: (
           attrs: ReturnType<typeof reactive<CommonShapeAttrs<PersonalAttrs>>>
-        ) => void;
-      } & ThisType<Image>
+        ) => void
+      } & ThisType<ImageRepeat>
     >
   ) {
-    super(attrs);
+    super(attrs)
 
-    this[SCOPE].on();
+    this[SCOPE].on()
 
-    // eslint-disable-next-line no-undef
     this._image = computed<CanvasImageSource>(() => {
-      const { image } = this.$;
+      // eslint-disable-next-line functional/no-let
+      let { image: _image } = this.$
 
-      if (typeof image === "string") return getImage(image);
+      if (typeof _image === "string") _image = getImage(_image)
 
-      return image;
-    });
-    watch(this._image, () => this._cacheImageRepeat.clear());
+      const { crop, width, height } = this.$
 
-    this[SCOPE].off();
+      // eslint-disable-next-line functional/no-let
+      let image: CanvasImageSource
+      if (crop) {
+        image = document.createElement("canvas")
+        image.width = width ?? crop.width
+        image.height = height ?? crop.height
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        image
+          .getContext("2d")!
+          .drawImage(
+            _image,
+            crop.x,
+            crop.y,
+            crop.width,
+            crop.height,
+            0,
+            0,
+            width ?? crop.width,
+            height ?? crop.height
+          )
+      } else if (width !== undefined && height !== undefined) {
+        image = document.createElement("canvas")
+        ;[image.width, image.height] = [width, height]
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        image
+          .getContext("2d")!
+          .drawImage(this._image.value, 0, 0, width, height)
+      } else {
+        image = _image
+        // image.drawImage(this._image.value, 0, 0);
+      }
+
+      return image
+    })
+    watch(this._image, () => this._cacheImageRepeat.clear())
+
+    this[SCOPE].off()
   }
 
   protected getSize() {
@@ -286,12 +310,20 @@ export class ImageRepeat extends Shape<PersonalAttrs> {
         this.$.scrollWidth ??
         this.$.width ??
         this.$.crop?.width ??
-        getValFromSource(this._image.value.width),
+        getValFromSource(
+          typeof this.$.image === "object"
+            ? this.$.image.width
+            : this._image?.value.width ?? 0
+        ),
       height:
         this.$.scrollHeight ??
         this.$.height ??
         this.$.crop?.height ??
-        getValFromSource(this._image.value.height),
-    };
+        getValFromSource(
+          typeof this.$.image === "object"
+            ? this.$.image.height
+            : this._image?.value.height ?? 0
+        )
+    }
   }
 }
