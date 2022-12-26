7148e7f88bb76320ec9c02ecba6fe3fa23d4e8ea
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-12-26 12:31:25 +0000

    rewrite api

--- packages/animate/src/index.ts
+++ packages/animate/src/index.ts
@@ -1,31 +1,10 @@
-import { Shape } from "fcanvas"
+import type { Shape } from "fcanvas"
 import gsap from "gsap"
 
-import type { PersonalAttrs } from "../../fcanvas/src/shapes/Arc"
-import type { CommonShapeAttrs } from "../../fcanvas/src/type/CommonShapeAttrs"
-
-declare module "fcanvas" {
-  export class Shape<
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    PersonalAttrs extends Record<string, unknown> = {}
-  > {
-    public to(
-      attrs: gsap.TweenVars &
-        Partial<CommonShapeAttrs<PersonalAttrs>> & {
-          keyframes: (gsap.TweenVars &
-            Partial<CommonShapeAttrs<PersonalAttrs>>)[]
-        }
-    ): gsap.core.Tween
+export function installAnimate(shape: typeof Shape) {
+  shape.prototype.to = function (attrs) {
+    return gsap.to(this.$, attrs)
   }
-}
-
-Shape.prototype.to = function (
-  attrs: gsap.TweenVars &
-    Partial<CommonShapeAttrs<PersonalAttrs>> & {
-      keyframes: (gsap.TweenVars & Partial<CommonShapeAttrs<PersonalAttrs>>)[]
-    }
-): gsap.core.Tween {
-  return gsap.to(this.$, attrs)
 }
 
 export { Animation } from "./Animation"
