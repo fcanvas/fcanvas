e973e70fc36057da858ae300a238a581ce5f1d53
Tachibana Shin <tachibshin@duck.com>
2023-02-26 10:12:17 +0000

    [communicate]: typo

--- packages/communicate/src/communicate.ts
+++ packages/communicate/src/communicate.ts
@@ -1,3 +1,4 @@
+/* eslint-disable functional/no-method-signature */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable functional/functional-parameters */
 /** @worker */
@@ -12,6 +13,23 @@ import { uuid } from "./logic/uuid"
 //   timeout: 0
 // })//: Promise<1343254>
 
+interface WindowPostMessageOptions {
+  targetOrigin?: string
+  transfer?: Transferable[]
+}
+interface LikeMessagePort {
+  addEventListener(
+    name: "message",
+    cb: (event: MessageEvent<any>) => void,
+    options?: unknown
+  ): void
+  removeEventListener(
+    name: "message",
+    cb: (event: MessageEvent<any>) => void,
+    options?: unknown
+  ): void
+  postMessage(message: any, options?: unknown): void
+}
 interface DataCallFn<Fn extends (...args: unknown[]) => unknown> {
   id: string
   type: "call_fn"
@@ -36,12 +54,12 @@ type DataReturnFn<Fn extends (...args: unknown[]) => unknown> =
 
 type MayBePromise<T> = T | Promise<T>
 function listen<Fn extends (...args: any[]) => any>(
-  port: MessagePort,
+  port: LikeMessagePort,
   name: string,
-  listener: (
-    ...args: Parameters<Fn>
-  ) => MayBePromise<
-    | [ReturnType<Fn>, (StructuredSerializeOptions | Transferable[])?]
+  listener: (...args: Parameters<Fn>) => MayBePromise<
+    | ({
+        return: ReturnType<Fn>
+      } & WindowPostMessageOptions)
     | ReturnType<Fn>
   >,
   options?: {
@@ -55,15 +73,22 @@ function listen<Fn extends (...args: any[]) => any>(
     if (data.type === "call_fn" && data.name === name) {
       // eslint-disable-next-line functional/no-let
       let result:
-        | [ReturnType<Fn>, (StructuredSerializeOptions | Transferable[])?]
+        | ({
+            return: ReturnType<Fn>
+          } & WindowPostMessageOptions)
         | undefined
       // eslint-disable-next-line functional/no-let
       let err: string | undefined
       try {
         const r = await listener(...data.args)
 
-        if (Array.isArray(r)) result = r
-        else result = [r]
+        if ("return" in r) {
+          result = r
+        } else {
+          result = {
+            return: r
+          }
+        }
       } catch (error) {
         err = error + ""
       }
@@ -74,7 +99,7 @@ function listen<Fn extends (...args: any[]) => any>(
         name: data.name,
         ...(result
           ? {
-              retu: result[0],
+              retu: result.return,
               isOk: true
             }
           : {
@@ -83,7 +108,7 @@ function listen<Fn extends (...args: any[]) => any>(
               isOk: false
             })
       }
-      port.postMessage(message, result?.[1] as unknown as any)
+      port.postMessage(message, result)
 
       if (options?.once) stop()
     }
@@ -98,32 +123,30 @@ function listen<Fn extends (...args: any[]) => any>(
 }
 
 function put<Fn extends (...args: any[]) => any>(
-  port: MessagePort,
+  port: LikeMessagePort,
   name: string,
   ...args: Parameters<Fn>
 ): Promise<ReturnType<Fn>>
 // eslint-disable-next-line no-redeclare
 function put<Fn extends (...args: any[]) => any>(
-  port: MessagePort,
+  port: LikeMessagePort,
   options: {
     name: string
     timeout?: number
-    transfer?: Transferable[]
     signal?: AbortSignal
-  },
+  } & WindowPostMessageOptions,
   ...args: Parameters<Fn>
 ): Promise<ReturnType<Fn>>
 // eslint-disable-next-line no-redeclare
 function put<Fn extends (...args: any[]) => any>(
-  port: MessagePort,
+  port: LikeMessagePort,
   options:
     | string
-    | {
+    | ({
         name: string
         timeout?: number
-        transfer?: Transferable[]
         signal?: AbortSignal
-      },
+      } & WindowPostMessageOptions),
   ...args: Parameters<Fn>
 ): Promise<ReturnType<Fn>> {
   return new Promise<ReturnType<Fn>>((resolve, reject) => {
@@ -134,13 +157,10 @@ function put<Fn extends (...args: any[]) => any>(
     // eslint-disable-next-line functional/no-let
     let timeout = 30_000
     // eslint-disable-next-line functional/no-let
-    let transfer: Transferable[] | undefined
-    // eslint-disable-next-line functional/no-let
     let signal: AbortSignal | undefined
     if (typeof options === "object") {
       name = options.name
       timeout = options.timeout ?? 30_000
-      transfer = options.transfer
       signal = options.signal
     } else {
       name = options
@@ -187,7 +207,7 @@ function put<Fn extends (...args: any[]) => any>(
       name,
       args
     }
-    port.postMessage(message, transfer as unknown as any)
+    port.postMessage(message, typeof options === "object" ? options : undefined)
   })
 }
 
