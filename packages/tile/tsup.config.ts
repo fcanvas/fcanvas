567588e20daa3583a0f55715ff2baa81115c296d
Tachibana Shin <tachibshin@duck.com>
2023-02-26 10:02:06 +0000

    fix build `iife`

--- packages/tile/tsup.config.ts
+++ packages/tile/tsup.config.ts
@@ -12,7 +12,8 @@ const configNormal: Options = {
   env: {
     IS_BROWSER: "false"
   },
-  external: ["fcanvas"]
+  external: ["fcanvas"],
+  globalName: "fTile"
 }
 const configBrowser: Options = {
   entry: {
