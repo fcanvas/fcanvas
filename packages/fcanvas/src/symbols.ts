8e3ffa7d51dda6b45a84b73043d0939479e81d72
Tachibana Shin <tachibshin@duck.com>
2023-03-02 22:07:38 +0700

    symbols

--- packages/fcanvas/src/symbols.ts
+++ packages/fcanvas/src/symbols.ts
@@ -1,16 +1,20 @@
-export const CONTEXT_CACHE = Symbol("context cache")
-export const COMPUTED_CACHE = Symbol("computed cache")
-export const DRAW_CONTEXT_ON_SANDBOX = Symbol("draw context on sandbox")
-export const BOUNCE_CLIENT_RECT = Symbol("bounce client rect")
-export const BOUNDING_CLIENT_RECT = Symbol("bounding client rect")
-export const CONTEXT_CACHE_SIZE = Symbol("context size")
-export const CHILD_NODE = Symbol("child node")
-export const CANVAS_ELEMENT = Symbol("canvas element")
-export const SCOPE = Symbol("scope")
-export const DIV_CONTAINER = Symbol("div container")
-export const LISTENERS = Symbol("listeners")
-export const LOCALS = Symbol("locals")
-export const STORE_EVENTS = Symbol("store events")
-export const ADD_EVENT = Symbol("add event")
-export const REMOVE_EVENT = Symbol("remove event")
-export const RAW_MAP_LISTENERS = Symbol("raw map listeners")
+export const CONTEXT_CACHE = Symbol(__DEV__ ? "context cache" : "");
+export const COMPUTED_CACHE = Symbol(__DEV__ ? "computed cache" : "");
+export const DRAW_CONTEXT_ON_SANDBOX = Symbol(
+  __DEV__ ? "draw context on sandbox" : ""
+);
+export const BOUNCE_CLIENT_RECT = Symbol(__DEV__ ? "bounce client rect" : "");
+export const BOUNDING_CLIENT_RECT = Symbol(
+  __DEV__ ? "bounding client rect" : ""
+);
+export const CONTEXT_CACHE_SIZE = Symbol(__DEV__ ? "context size" : "");
+export const CHILD_NODE = Symbol(__DEV__ ? "child node" : "");
+export const CANVAS_ELEMENT = Symbol(__DEV__ ? "canvas element" : "");
+export const SCOPE = Symbol(__DEV__ ? "scope" : "");
+export const DIV_CONTAINER = Symbol(__DEV__ ? "div container" : "");
+export const LISTENERS = Symbol(__DEV__ ? "listeners" : "");
+export const LOCALS = Symbol(__DEV__ ? "locals" : "");
+export const STORE_EVENTS = Symbol(__DEV__ ? "store events" : "");
+export const ADD_EVENT = Symbol(__DEV__ ? "add event" : "");
+export const REMOVE_EVENT = Symbol(__DEV__ ? "remove event" : "");
+export const RAW_MAP_LISTENERS = Symbol(__DEV__ ? "raw map listeners" : "");
