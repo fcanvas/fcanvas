5812f93f0466e5b36cc6aa7a819bc75ca44d9b8d
tachibana-shin <tachibshin@duck.com>
2023-03-07 15:05:06 +0000

    Deploying to gh-pages from @ tachibana-shin/fcanvas-next@57854b17b14251d9500f6506dcc281e7afb80146 🚀

---
+++ assets/guide_shapes_Tag.md.3f2a9622.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as a,b as d}from"./app.a8a05f9f.js";const g=JSON.parse('{"title":"Tag Shape","description":"","frontmatter":{},"headers":[],"relativePath":"guide/shapes/Tag.md","lastUpdated":1678201361000}'),o={name:"guide/shapes/Tag.md"},r=d("",8),s=[r];function i(n,c,h,p,l,u){return e(),a("div",null,s)}const b=t(o,[["render",i]]);export{g as __pageData,b as default};
--- assets/guide_shapes_Tag.md.908abcd4.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as a,b as d}from"./app.024f0f8e.js";const g=JSON.parse('{"title":"Tag Shape","description":"","frontmatter":{},"headers":[],"relativePath":"guide/shapes/Tag.md","lastUpdated":1678194870000}'),o={name:"guide/shapes/Tag.md"},r=d("",8),s=[r];function i(n,c,h,p,l,u){return e(),a("div",null,s)}const b=t(o,[["render",i]]);export{g as __pageData,b as default};
