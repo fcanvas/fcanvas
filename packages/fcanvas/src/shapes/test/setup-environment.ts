0aa5b5531cae0b1b9155a6d876095c5c9e872915
Tachibana Shin <tachibshin@duck.com>
2023-03-08 09:20:39 +0000

    ci

--- packages/fcanvas/src/shapes/test/setup-environment.ts
+++ packages/fcanvas/src/shapes/test/setup-environment.ts
@@ -2,8 +2,8 @@ import { DOMMatrix, Image, loadImage } from "canvas"
 
 import { CONFIGS } from "../../configs"
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-;(self as any).DOMMatrix = DOMMatrix;
+;(self as any).DOMMatrix = DOMMatrix
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-(Image.prototype as any)[Symbol.toStringTag] = "Image"
+;(Image.prototype as any)[Symbol.toStringTag] = "Image"
 
 CONFIGS.loadImage = loadImage
